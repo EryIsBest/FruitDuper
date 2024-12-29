03ba5a23172bc28eedc37dfc2cbabcec196be79f
EryIsBest <115062021+EryIsBest@users.noreply.github.com>
2024-12-29 23:32:36 +1100

    Update LOADING_UI.lua

--- script/LOADING_UI.lua
+++ script/LOADING_UI.lua
@@ -1,6 +1,6 @@
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
-local Password = "savage1444"  -- Password for accessing the UI
+local Password = "123"  -- Password for accessing the UI
 
 -- Create a Password Input Window
 local function showPasswordWindow()
